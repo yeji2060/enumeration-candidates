518e0647d9ad98c52c6d5a30315019eab6230b52
Yeji Ha <yeji206000@gmail.com>
2024-05-31 23:58:27 -0700

    implement the functions in filter.rb

--- filters.rb
+++ filters.rb
@@ -2,15 +2,25 @@
 # This way, we keep these methods separated from other potential parts of the program
 
 def find(id)
-    # Your code Here
-  end
+  $candidates.find { |candidate| candidate[:id] == id}
+end
   
-  def experienced?(candidate)
-    # Your code Here
+def experienced?(candidate)
+  candidate[:years_of_experience] >= 2
+end
+
+def qualified_candidates(candidates)
+  candidates.select do |candidate|
+    experienced?(candiate)&&
+    candidate[:github_points] >= 100 &&
+    (candidate[:languages].include?('Ruby') || candidate[:languages].include?('Python')) &&
+    candidate[:date_applied] >= 15.days.ago.to_date &&
+    candidate[:age] > 17
   end
+end
   
-  def qualified_candidates(candidates)
-    # Your code Here
-  end
-  
+def ordered_by_qualifications(candidates)
+  candidates.sort_by { |candidate| [-candidate[:years_of_experience], -candiate[:github_points]]}
+end
+
   # More methods will go below
