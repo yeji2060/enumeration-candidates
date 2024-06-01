8f1cfb85186d3069855d0ffd3383662201573a22
yeji2060 <115120934+yeji2060@users.noreply.github.com>
2024-05-31 23:37:52 -0700

    Initial commit

---
+++ main.rb
@@ -0,0 +1,9 @@
+# This is the main entrypoint into the program
+# It requires the other files/gems that it needs
+require 'pp'
+require './candidates'
+require './filters'
+
+## Your test code can go here
+
+pp @candidates
